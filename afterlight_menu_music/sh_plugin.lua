1bce725aa70d954122a7134bab84cead4b8ac672
Barbiestalin <washington8567@gmail.com>
2026-09-09 15:02:17 +0300

    Delete afterlight_menu_music directory

--- afterlight_menu_music/sh_plugin.lua
+++
@@ -1,9 +0,0 @@
-PLUGIN.name = "Afterlight Menu Music"
-PLUGIN.author = "Afterlight"
-PLUGIN.description = "Controls looping music for intro, character menu and Helix menu."
-
-if (SERVER) then
-	resource.AddFile("sound/afterlight/intro_music.mp3")
-end
-
-ix.util.Include("cl_plugin.lua")
